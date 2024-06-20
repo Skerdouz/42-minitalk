b7d084800536ee8b741d8f17288d322dbd458c79
Skerdouz <lucasbrahinski60@gmail.com>
2024-06-21 00:48:32 +0200

    not important

--- include/minitalk.h
+++ include/minitalk.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbrahins <lbrahins@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/21 00:35:07 by lbrahins          #+#    #+#             */
+/*   Updated: 2024/06/21 00:44:54 by lbrahins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINITALK_H
 # define MINITALK_H
 
@@ -7,8 +19,8 @@
 
 # include <unistd.h>		/* getpid() */
 # include <sys/types.h>		/* pid_t */
-# include <signal.h>		/* kill() */
-# include <stdlib.h>		/* malloc() */
+# include <signal.h>		/* kill(), sigaction() */
+# include <stdlib.h>		/* malloc(), exit() */
 
 # include "libft.h"
 
