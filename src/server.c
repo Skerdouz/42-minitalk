c5a5c2ba81849c50db101696a87400bae9375b5a
Skerdouz <lucasbrahinski60@gmail.com>
2024-06-21 00:49:11 +0200

    big changes to server.c

--- src/server.c
+++ src/server.c
@@ -1,68 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbrahins <lbrahins@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/21 00:34:49 by lbrahins          #+#    #+#             */
+/*   Updated: 2024/06/21 00:43:35 by lbrahins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
 
-t_data	g_data;
+/*
+*	TODO
+*	- Add the ability to send signals to the client
+*	- Try to optimize stuff
+*/
 
-void	reset_data(void)
+static char	*init_msg(void)
 {
-	g_data.bit = 0;
-	g_data.byte = 0;
-	g_data.client_pid = 0;
+	char	*msg;
+
+	msg = (char *)malloc(1 * sizeof(char));
+	if (!msg)
+		exit(EXIT_FAILURE);
+	return (msg[0] = '\0', msg);
 }
 
-static unsigned char	reverse_byte(unsigned char byte)
+static char	*msg_realloc(char *old, unsigned char c)
 {
-	unsigned char	result;
-	int				i;
+	int		len;
+	int		i;
+	char	*new;
 
-	result = 0;
-	i = 0;
-	while (i++ < 8)
-	{
-		result <<= 1;
-		result |= (byte & 1);
-		byte >>= 1;
-	}
-	return (result);
+	len = ft_strlen(old);
+	new = (char *)malloc((len + 2) * sizeof(char));
+	if (!new)
+		return (NULL);
+	i = -1;
+	while (++i < len)
+		new[i] = old[i];
+	new[i++] = c;
+	new[i] = '\0';
+	return (free(old), new);
+}
+
+static char	*newchar_handler(char *msg, unsigned char c)
+{
+	if (!c)
+		return (ft_printf("<client> %s\n", msg), free(msg), NULL);
+	return (msg_realloc(msg, c));
 }
 
 static void	sighandler(int signum, siginfo_t *info, void *context)
 {
-	(void)context; /*gcc error handler*/
-	if (g_data.client_pid != info->si_pid)
+	static int		c;
+	static int		bit;
+	static char		*msg;
+
+	(void)context;
+	(void)info;/* don't forget to remove that stuff after handling the answer to the client */
+	if (!msg)
+		msg = init_msg();
+	if (signum == SIGUSR2)
+		c |= (1 << bit);
+	if (++bit == 8)
 	{
-		reset_data();
-		g_data.client_pid = info->si_pid;
+		msg = newchar_handler(msg, (unsigned char)c);
+		c = 0;
+		bit = 0;
 	}
-	if (signum == SIGUSR1)
-		g_data.byte = (g_data.byte << 1) | 1;
-	else if (signum == SIGUSR2)
-	{
-		g_data.byte <<= 1;
-	}
-	if (++g_data.bit == 8)
-	{
-		if (is_end(g_data.byte))
-			printstash();
-		g_data.byte = reverse_byte(g_data.byte);
-		write(1, &g_data.byte, 1);
-		reset_data();
-	}
+	// kill(info->si_pid, signum);
 }
 
 int	main(void)
 {
-	struct sigaction	sa;
+	struct sigaction	sig;
 	pid_t				pid;
 
 	pid = getpid();
-	if (pid <= 0)
-		return (ft_putstr_fd("error: invalid PID\n", 1), 1);
-	reset_data();
-	ft_printf("pid: %u\n", pid);
+	ft_printf("<server> Server's PID: %d\n", pid);
+	sig.sa_sigaction = sighandler;
+	sigemptyset(&sig.sa_mask);
+	sig.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGUSR1, &sig, NULL) == -1 || sigaction(SIGUSR2, &sig, NULL) == -1)
+		return (ft_putstr_fd("error: sigaction\n", 1), 1);
 	while (1)
-		usleep(1200);
+		usleep(USLEEP_T);
 	return (0);
 }
-
-
-
