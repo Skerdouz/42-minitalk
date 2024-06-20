c96435efc335155e5363adfb8bda1df8c624bc8a
Skerdouz <lucasbrahinski60@gmail.com>
2024-06-21 00:49:40 +0200

    wip todo list

--- src/client.c
+++ src/client.c
@@ -1,13 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbrahins <lbrahins@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/21 00:34:53 by lbrahins          #+#    #+#             */
+/*   Updated: 2024/06/21 00:38:23 by lbrahins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
+
+/*
+*	TODO
+*	- Optimize stuff
+*	- Check error handling
+*	- Handle the server answer signals
+*	- Add a timeout? maybe
+*/
 
 static void	sighandler(int signum)
 {
 	(void)signum;
 }
+static void	send_null_signal(pid_t pid)
+{
+	int	bit;
+
+	bit = 0;
+	while (bit++ < 8)
+	{
+		if (kill(pid, SIGUSR1) == -1)
+			exit(1);
+		usleep(USLEEP_T);
+	}
+}
 
 static void	send_signal(pid_t pid, char *str)
 {
-	int					bit;
+	int	bit;
 
 	bit = 0;
 	while (*str)
@@ -17,18 +49,19 @@ static void	send_signal(pid_t pid, char *str)
 		{
 			if (*str & (1 << bit++))
 			{
-				if (kill(pid, SIGUSR1) == -1)
+				if (kill(pid, SIGUSR2) == -1)
 					exit(1);
 			}
 			else
 			{
-				if (kill(pid, SIGUSR2) == -1)
+				if (kill(pid, SIGUSR1) == -1)
 					exit(1);
 			}
-			usleep(1200);
+			usleep(USLEEP_T);
 		}
 		str++;
 	}
+	send_null_signal(pid);
 }
 
 int	main(int ac, char **av)
@@ -39,21 +72,18 @@ int	main(int ac, char **av)
 
 	if (--ac != 2)
 	{
-		ft_printf("Invalid arguments, expected: arg1:<pid> arg2:<string>\n");
+		ft_putstr_fd("Invalid arguments, expected: ./client [(int)pid] [(string)message]\n", 1);
 		exit(0);
 	}
 	pid = ft_atoi(av[1]);
 	if (pid <= 0)
-	{
-		ft_printf("Invalid PID\n");
-		exit(0);
-	}
+		return (ft_putstr_fd("error: invalid PID\n", 1), 1);
 	if (!av[2])
 		return (1);
 	ft_memset(&sa, 0, sizeof(sa));
 	sa.sa_handler = sighandler;
 	sigemptyset(&sa.sa_mask);
-	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
 		return (1);
 	send_signal(pid, av[2]);
 	return (0);
